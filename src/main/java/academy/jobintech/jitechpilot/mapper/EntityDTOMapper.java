b10b7ff2cce16eb8bab88cfcc95e3853a995043f
Rida El Ayadi <rdlzaar@gmail.com>
2024-03-01 11:58:27 +0100

    fixing errors

--- src/main/java/academy/jobintech/jitechpilot/mapper/EntityDTOMapper.java
+++ src/main/java/academy/jobintech/jitechpilot/mapper/EntityDTOMapper.java
@@ -1,25 +1,7 @@
-<<<<<<< src/main/java/academy/jobintech/jitechpilot/mapper/EntityDTOMapper.java
 package academy.jobintech.jitechpilot.mapper;
 
 import java.util.List;
 
-public interface EntityDTOMapper<E,D>{
-    D toDto(E entity);
-    E toEntity(D dto);
-
-    default List<D> toDtos(List<E> entities){
-        if(entities==null) return null;
-        return  entities.stream().map(this::toDto).toList();
-    }
-
-    default List<E> toEntities(List<D> dtos){
-        if(dtos==null) return null;
-        return  dtos.stream().map(this::toEntity).toList();
-    }
-
-}
-=======
-package academy.jobintech.jitechpilot.mapper;
 
 import java.util.List;
 
@@ -38,4 +20,4 @@ public interface EntityDTOMapper <E,D>{
         return  dtos.stream().map(this::toEntity).toList();
     }
 }
->>>>>>> src/main/java/academy/jobintech/jitechpilot/mapper/EntityDTOMapper.java
+

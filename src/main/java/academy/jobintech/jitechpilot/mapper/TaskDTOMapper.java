4ec0371113112a6ee92b409643b7d6bf1a0459fd
unknown <fatimazahraboudad9@gmail.com>
2024-03-04 13:13:37 +0100

    using ModelMapper in task & ticket

--- src/main/java/academy/jobintech/jitechpilot/mapper/TaskDTOMapper.java
+++ src/main/java/academy/jobintech/jitechpilot/mapper/TaskDTOMapper.java
@@ -2,6 +2,7 @@ package academy.jobintech.jitechpilot.mapper;
 
 import academy.jobintech.jitechpilot.dto.TaskDTO;
 import academy.jobintech.jitechpilot.entity.Task;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -12,18 +13,24 @@ import org.springframework.stereotype.Component;
 @Scope(value = "singleton")
 public class TaskDTOMapper implements EntityDTOMapper<Task, TaskDTO>{
 
+    private final ModelMapper modelMapper;
+
+    public TaskDTOMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
 
     @Override
     public TaskDTO toDto(Task task) {
 
-        TaskDTO dto = new TaskDTO();
-
-        dto.setTaskId(task.getTaskId());
-        dto.setTitle(task.getTitle());
-        dto.setDescription(task.getDescription());
-        dto.setStatus(task.getStatus());
-        dto.setDeadline(task.getDeadline());
-        //dto.setTicketId(task.getTicket().getTicketId());
+        TaskDTO dto = modelMapper.map(task, TaskDTO.class);
+//        new TaskDTO();
+//
+//        dto.setTaskId(task.getTaskId());
+//        dto.setTitle(task.getTitle());
+//        dto.setDescription(task.getDescription());
+//        dto.setStatus(task.getStatus());
+//        dto.setDeadline(task.getDeadline());
+//        //dto.setTicketId(task.getTicket().getTicketId());
 
         return dto;
     }
@@ -31,12 +38,13 @@ public class TaskDTOMapper implements EntityDTOMapper<Task, TaskDTO>{
     @Override
     public Task toEntity(TaskDTO dto) {
 
-        Task task = new Task();
-
-        task.setTitle(dto.getTitle());
-        task.setDescription(dto.getDescription());
-        task.setStatus(dto.getStatus());
-        task.setDeadline(dto.getDeadline());
+        Task task =modelMapper.map(dto, Task.class);
+//        new Task();
+//
+//        task.setTitle(dto.getTitle());
+//        task.setDescription(dto.getDescription());
+//        task.setStatus(dto.getStatus());
+//        task.setDeadline(dto.getDeadline());
 
         return task;
     }

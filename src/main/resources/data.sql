81c44ef11054fb8104a27f5e29c199ecd727f91c
Achraf Lamhamdi <achraflamhamdi@gmail.com>
2024-03-21 11:58:16 +0000

    update data.sql with sprint queries

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -18,8 +18,6 @@ INSERT INTO board (board_id, board_name, description, start_date, access_level, 
 INSERT INTO board (board_id, board_name, description, start_date, access_level, fav, workspace_id_board) VALUES (4, 'Sales Targets', 'Description of Sales Targets', NOW(), 'PUBLIC', false, 4);
 INSERT INTO board (board_id, board_name, description, start_date, access_level, fav, workspace_id_board) VALUES (5, 'Onboarding', 'Description of Onboarding', NOW(), 'PUBLIC', false, 5);
 INSERT INTO board (board_id, board_name, description, start_date, access_level, fav, workspace_id_board) VALUES (6, 'Product Launch', 'Description of Product Launch', NOW(), 'PUBLIC', false, 6);
-INSERT INTO board (board_id, board_name, description, start_date, access_level, fav, workspace_id_board) VALUES (7, 'Product Deployment', 'Description of Product Deployment', NOW(), 'PUBLIC', false, 6);
-INSERT INTO board (board_id, board_name, description, start_date, access_level, fav, workspace_id_board) VALUES (8, 'Product Dev.', 'Description of Product Development', NOW(), 'PUBLIC', false, 6);
 
 INSERT INTO section (section_id, section_title, board_id_section) VALUES (1, 'Backlog ',  1);
 INSERT INTO section (section_id, section_title, board_id_section) VALUES (2, 'In Progress ', 2);
@@ -27,33 +25,24 @@ INSERT INTO section (section_id, section_title, board_id_section) VALUES (3, 'Re
 INSERT INTO section (section_id, section_title, board_id_section) VALUES (4, 'Done ', 4);
 INSERT INTO section (section_id, section_title, board_id_section) VALUES (5, 'Backlog ',5);
 INSERT INTO section (section_id, section_title, board_id_section) VALUES (6, 'In Progress',6);
-INSERT INTO section (section_id, section_title, board_id_section) VALUES (7, 'TODO',8);
-INSERT INTO section (section_id, section_title, board_id_section) VALUES (8, 'IN PROGRESS',8);
-INSERT INTO section (section_id, section_title, board_id_section) VALUES (9, 'DONE',8);
 
-INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority, section_id_ticket) VALUES (1, 'Fix login bug', 'Description of Fix login bug ', NOW(), DATE('2024-12-31 01:02:03'), 'HIGH', 1);
-INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority, section_id_ticket) VALUES (2, 'Update landing page', 'Description of Update landing page', NOW(), DATE('2024-12-31 01:02:03'), 'HIGH', 2);
-INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority, section_id_ticket) VALUES (3, 'Create new user flow', 'Description of Create new user flow ',NOW(), DATE('2024-12-31 01:02:03'),  'HIGH', 3);
-INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority, section_id_ticket) VALUES (4, 'Improve SEO ', 'Description of Improve SEO ', NOW(), DATE('2024-12-31 01:02:03'),'HIGH',  4);
-INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority, section_id_ticket) VALUES (5, 'Refactor backend code', 'Description of Refactor backend code ', NOW(), DATE('2024-12-31 01:02:03'), 'HIGH', 5);
-INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority, section_id_ticket) VALUES (6, 'Fix login bug ', 'Description of Fix login bug ', NOW(), DATE('2024-12-31 01:02:03'), 'HIGH', 6);
-INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority, section_id_ticket) VALUES (7, 'Build Home Page ', 'Description of Building home page ', NOW(), DATE('2024-12-31 01:02:03'), 'HIGH', 7);
-INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority, section_id_ticket) VALUES (8, 'User Authentication', 'Description of User Authentication ', NOW(), DATE('2024-12-31 01:02:03'), 'HIGH', 7);
-INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority, section_id_ticket) VALUES (9, 'Fix search bar ', 'Description of Fix search bar ', NOW(), DATE('2024-12-31 01:02:03'), 'HIGH', 7);
+INSERT INTO sprint (sprint_id , sprint_number, start_date, end_date, target_velocity, board_id_sprint) VALUES (1,1, NOW(), NOW(), 50, 1);
+INSERT INTO sprint (sprint_id , sprint_number, start_date, end_date, target_velocity, board_id_sprint) VALUES (2,2, NOW(), NOW(), 50, 1);
+INSERT INTO sprint (sprint_id , sprint_number, start_date, end_date, target_velocity, board_id_sprint) VALUES (3,3, NOW(), NOW(), 50, 3);
+
+INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority,complexity_points, section_id_ticket , sprint_id_ticket) VALUES (1, 'Fix login bug', 'Description of Fix login bug ', NOW(), DATE('2024-12-31 01:02:03'), 'HIGH',5, 1 ,1);
+INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority,complexity_points, section_id_ticket ,sprint_id_ticket) VALUES (2, 'Update landing page', 'Description of Update landing page', NOW(), DATE('2024-12-31 01:02:03'), 'HIGH',4, 2 ,1);
+INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority,complexity_points, section_id_ticket ,sprint_id_ticket) VALUES (3, 'Create new user flow', 'Description of Create new user flow ',NOW(), DATE('2024-12-31 01:02:03'),  'HIGH',2, 3,2);
+INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority,complexity_points, section_id_ticket , sprint_id_ticket) VALUES (4, 'Improve SEO ', 'Description of Improve SEO ', NOW(), DATE('2024-12-31 01:02:03'),'HIGH',3,  4,2);
+INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority,complexity_points, section_id_ticket , sprint_id_ticket) VALUES (5, 'Refactor backend code', 'Description of Refactor backend code ', NOW(), DATE('2024-12-31 01:02:03'), 'HIGH',7, 5,2);
+INSERT INTO ticket (ticket_id, title, description, start_date, end_date, priority,complexity_points, section_id_ticket , sprint_id_ticket) VALUES (6, 'Fix login bug ', 'Description of Fix login bug ', NOW(), DATE('2024-12-31 01:02:03'), 'HIGH',3, 6,2);
 
 INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (1, 'Write unit tests', false, 1);
-INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (7, 'Write integration tests', false, 1);
-INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (8, 'Logback', false, 1);
-INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (9, 'Authentication JWT', false, 1);
-INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (10, 'API Security', false, 1);
 INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (2, 'Design new feature', false, 2);
 INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (3, 'Code review', false, 3);
 INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (4, 'Database optimization', false, 4);
 INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (5, 'Update documentation ', false, 5);
 INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (6, 'Write unit tests ', false, 6);
-INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (13, 'Database Conception', false, 7);
-INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (14, 'UI/UX Design', false, 7);
-INSERT INTO task (task_id, title, is_done, ticket_id_task) VALUES (15, 'Project Init.', false, 7);
 
 INSERT INTO user_board_role (user_id, board_id, user_role) VALUES (1, 1, 'MEMBER');
 INSERT INTO user_board_role (user_id, board_id, user_role) VALUES (2, 2, 'MEMBER');
